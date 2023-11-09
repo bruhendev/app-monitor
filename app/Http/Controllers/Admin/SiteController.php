03f5e4b00dd30cd4831fd86a6004311d4bb8def9
Bruno Henrique <brunohenrique242@gmail.com>
2023-11-09 16:13:23 -0300

    chore: create controller SiteController

---
+++ app/Http/Controllers/Admin/SiteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Site;
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+        return Site::all();
+    }
+}

d967f536d5af9cec359074d3e92acdbfdb13e794
Bruno Henrique <brunohenrique242@gmail.com>
2023-11-09 13:19:25 -0300

    fix: update models relationship

--- app/Models/Check.php
+++ app/Models/Check.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Check extends Model
 {
     use HasFactory;
 
     protected $fillable = ['status_code', 'body'];
+
+    public function endpoint(): BelongsTo
+    {
+        return $this->belongsTo(Endpoint::class);
+    }
 }
